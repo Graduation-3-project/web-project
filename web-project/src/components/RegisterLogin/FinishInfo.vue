9eb982894b02a27422b97a51edf8c93e0b354667
i55 <i@DESKTOP-G7TCLEJ>
2020-04-22 18:39:14 +0800

    ip获取

--- web-project/src/components/RegisterLogin/FinishInfo.vue
+++ web-project/src/components/RegisterLogin/FinishInfo.vue
@@ -6,6 +6,10 @@
       :options="options"
       filterable
     ></el-cascader>
+    
+    <p>
+      {{ip}}
+    </p>
   </div>
 </template>
 
@@ -37,7 +41,8 @@
                 label: 'Form'
                  },
                 ]
-            },]
+            },],
+            ip: ''
           };
       },
       methods:{
@@ -54,7 +59,40 @@
 
           }, 300);
         },
+        getUserIP(onNewIP) {
 
+
+          let MyPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+          let pc = new MyPeerConnection({
+            iceServers: []
+          });
+          let noop = () => {
+          };
+          let localIPs = {};
+          let ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g;
+          let iterateIP = (ip) => {
+            if (!localIPs[ip]) onNewIP(ip);
+            localIPs[ip] = true;
+          };
+          pc.createDataChannel('');
+          pc.createOffer().then((sdp) => {
+            sdp.sdp.split('\n').forEach(function (line) {
+              if (line.indexOf('candidate') < 0) return;
+              line.match(ipRegex).forEach(iterateIP);
+            });
+            pc.setLocalDescription(sdp, noop, noop);
+          }).catch((reason) => {
+          });
+          pc.onicecandidate = (ice) => {
+            if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+            ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+          };
+        }
+      },
+      mounted(){
+        this.getUserIP((ip) => {
+          this.ip = ip;
+        });
       }
     }
 </script>
